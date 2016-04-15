8d70104df9d48fe4a91bb96343e643e26f822f0a
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-04-15 17:28:35 +0700

    Add converter implement

--- RoomForRentWindow/AvailableTimeColorMap.cs
+++ RoomForRentWindow/AvailableTimeColorMap.cs
@@ -7,14 +7,7 @@
         public string DefaultColor1 { get; set; }
         public string DefaultColor2 { get; set; }
         #endregion
-    }
 
-    public class DaysColor
-    {
-        #region  Properties & Indexers
-        public string Color1 { get; set; }
-        public string Color2 { get; set; }
-        public int Days { get; set; }
-        #endregion
+
     }
 }
---
+++ RoomForRentWindow/DaysColor.cs
@@ -0,0 +1,11 @@
+namespace RoomForRentWindow
+{
+    public class DaysColor
+    {
+        #region  Properties & Indexers
+        public string Color1 { get; set; }
+        public string Color2 { get; set; }
+        public int Days { get; set; }
+        #endregion
+    }
+}
