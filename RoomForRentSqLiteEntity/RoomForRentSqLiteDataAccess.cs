25359df887bb7cbc304204d3d837d35dc75f9093
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-04-19 17:39:27 +0700

    Add RoomForRentSqLiteDataAccess

---
+++ RoomForRentSqLiteEntity/RoomForRentSqLiteDataAccess.cs
@@ -0,0 +1,15 @@
+﻿using RoomForRentEntityDataAccess;
+
+
+namespace RoomForRentSqLiteEntity
+{
+    public class RoomForRentSqLiteDataAccess: RoomForRentEntityCommonDataAccess
+    {
+        #region Override
+        protected override RoomForRentEntityContext CreateContext()
+        {
+            return new RoomForRentSqlLiteEntityDbContext();
+        }
+        #endregion
+    }
+}
